56578010c53d5c4270fddb445ef2899ceff4588b
Sacconee <sacchetti.lorenzo09@icloud.com>
2026-09-24 12:49:55 +0200

    wd

--- 00_initial/es_1_brani.py
+++ 00_initial/es_1_brani.py
@@ -6,14 +6,17 @@ a = 67
 class Brano:
     # Creazione di un metodo/funzione, in questo caso con il costruttore
     def __init__(self, autore: str, titolo: str, durata: int): #questi sono i parametri di una funzione
+        self.autore = None
+        self.titolo = None
+        self.durata = None
         self.autore = autore
         self.titolo = titolo
         self.durata = durata
     
-    def __str__(self)
+    def __str__(self):
         return f"{self.titolo}"
 
 
 if __name__ == "__main__":
     newBrano = Brano("Gue", "Scooteroni", "180")
-    print(newBrano)    
+    print(newBrano)         
